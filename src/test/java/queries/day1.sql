a3f4076294c8c5f2703a514b2253076dd6331333
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-04-19 21:46:56 +0300

    project created

---
+++ src/test/java/queries/day1.sql
@@ -0,0 +1,29 @@
+select * from JOB_HISTORY;
+
+select JOB_ID from JOB_HISTORY;
+
+-- ctrl + enter
+
+select START_DATE, END_DATE, EMPLOYEE_ID from JOB_HISTORY;
+
+select  FIRST_NAME from EMPLOYEES;
+
+select distinct FIRST_NAME from EMPLOYEES;
+
+select count(FIRST_NAME) from EMPLOYEES;
+
+select count(COMMISSION_PCT) from EMPLOYEES;
+
+select distinct SALARY from EMPLOYEES;
+
+select SALARY from EMPLOYEES
+where
+     SALARY > 10000;
+
+select SALARY from EMPLOYEES
+where SALARY <> 2100;
+
+select SALARY from EMPLOYEES
+where SALARY != 2100;
+
+
